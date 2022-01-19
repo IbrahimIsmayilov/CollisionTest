9e3d30396f91d9470c096b230e52dc52948ae2a1
IbrahimIsmayilov <90726322+IbrahimIsmayilov@users.noreply.github.com>
2022-01-18 17:00:32 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -8,19 +8,29 @@ cnv.height = 600;
 
 // Global Variables
 let mouseX, mouseY;
-let xOne = Math.random() * 800;
-let xTwo = Math.random() * 800;
-let yOne = Math.random() * 600;
-let yTwo = Math.random() * 600;
-let widthOne = Math.random() * 40;
-let widthTwo = Math.random() * 40;
-let widthThree = Math.random() * 40;
-let heightOne = Math.random() * 40;
-let heightTwo = Math.random() * 40;
-let heightThree = Math.random() * 40;
 
+let mouseBox = {
+    w: Math.random() * 60 + 10,
+    h: Math.random() * 60 + 10,
+}
 
+let testBox1 = {
+    x: Math.random() * 600,
+    y: Math.random() * 600,
+    w: Math.random() * 60 + 10,
+    h: Math.random() * 60 + 10,
+}
+
+let testBox2 = {
+    x: Math.random() * 600,
+    y: Math.random() * 600,
+    w: Math.random() * 60 + 10,
+    h: Math.random() * 60 + 10,
+}
+
+// Event Listener
 document.addEventListener("mousemove", mousemoveHandler)
+
 function mousemoveHandler(event) {
     // Update mouseX and mouseY
     let cnvRect = cnv.getBoundingClientRect()
@@ -30,10 +40,16 @@ function mousemoveHandler(event) {
 
 requestAnimationFrame(draw);
 function draw() {
-    ctx.fillStyle = "black";
-    ctx.fillRect(mouseX, mouseY, widthOne, heightOne);
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillRect(xOne, yOne, widthTwo, heightTwo);
-    ctx.fillRect(xTwo, yTwo, widthThree, heightThree);
+    ctx.fillStyle = "black";
+    ctx.fillRect(mouseX, mouseY, mouseBox.w, mouseBox.h);
+
+    ctx.fillRect(testBox1.x, testBox1.y, testBox1.w, testBox1.h);
+    ctx.fillRect(testBox2.x, testBox2.y, testBox2.w, testBox2.h);
+    if (mouseX + mouseBox.w > testBox1.x && mouseX < testBox1.x + testBox1.w && mouseY + mouseBox.h > testBox1.y && mouseY < testBox1.y + testBox1.h || mouseX + mouseBox.w > testBox2.x && mouseX < testBox2.x + testBox2.w && mouseY + mouseBox.h > testBox2.y && mouseY < testBox2.y + testBox2.h ) {
+        console.log("collision");
+    }
     requestAnimationFrame(draw);
 }
